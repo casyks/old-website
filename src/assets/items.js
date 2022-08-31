3f317ffdbc22d68743ff9861c8279e104ab4fbbc
Casy <ozgurcan.ogulmus@outlook.com>
2022-08-31 13:35:28 +0300

    added be.net profile and edited texts

--- src/assets/items.js
+++ src/assets/items.js
@@ -1,46 +1,52 @@
 import instagram from '../assets/instagram.png'
 import youtube from '../assets/youtube.jpeg'
+import behance from '../assets/behance.png'
 import dribbble from '../assets/dribbble.png'
 import github from '../assets/github.png'
 import spotify from '../assets/spotify.png'
 import twitter from '../assets/twitter.png'
 
 const items = [{
+    "title": "Behance",
+    "subtitle": "my Behance page where I share the stylish looking designs I made!",
+    "image": behance,
+    "link": "http://be.net/casyks" //behance profile link 
+},
+{
+    "title": "Dribbble",
+    "subtitle": "like behance some design shots in here too!",
+    "image": dribbble,
+    "link": "https://dribbble.com/casyks" // Dribbble profile link 
+},
+    {
     "title": "GitHub",
-    "subtitle": "@casyks",
+    "subtitle": "my Github profile with this website and many more projects.",
     "image": github,
     "link": "https://github.com/casyks" //Github Profile link
 },
 {
     "title": "Instagram",
-    "subtitle": "@casyks",
+    "subtitle": "my personal Instagram page!",
     "image": instagram,
     "link": "https://instagram.com/casyks" //instagram profile link 
 },
 {
     "title": "Spotify",
-    "subtitle": "casyks Artist Profile",
+    "subtitle": "my Spotify Artist page where I share my songs!",
     "image": spotify,
     "link": "https://open.spotify.com/artist/1fHGp2TTdSU1jVQymk6dFD" //spotify link 
 },
 {
     "title": "Twitter",
-    "subtitle": "@casyks | 😉",
+    "subtitle": "just for nick actually, nothing much but you can follow me!",
     "image": twitter,
     "link": "https://twitter.com/casyks"// twitter profile link 
 },
 {
     "title": "YouTube",
-    "subtitle": "casyks | soon will be oac",
+    "subtitle": "official artist Youtube channel",
     "image": youtube,
     "link": "https://www.youtube.com/channel/UC98QgaolcJz_T8OIrDtppfA"//youtube channel link 
-},
-
-{
-    "title": "Dribbble",
-    "subtitle": "@casyks | some design shots ",
-    "image": dribbble,
-    "link": "https://dribbble.com/casyks" // Dribbble profile link 
 }]
 
 export default items
